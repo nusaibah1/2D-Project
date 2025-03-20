5f56699c184ed40ce4e360ed50d35984f5d3d61b
Nusaibah1 <charifnusaibah01@gmail.com>
2025-03-20 14:40:05 +0200

    deploy fix

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceX: 39,
   sliceY: 31,
   anims: {
-    "idle-down": 936, // Change this to the correct index for the brown girl
+    "idle-down": 936,
     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
     "idle-side": 975,
     "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
@@ -16,7 +16,6 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
 });
 
 k.loadSprite("map", "./map.png");
-
 
 k.setBackground(k.Color.fromHex("#311047"));
 
