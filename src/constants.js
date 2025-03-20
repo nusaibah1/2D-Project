7c04c0f939232f196c2145b28c66b81c7a22e9c9
Nusaibah1 <charifnusaibah01@gmail.com>
2025-03-20 14:09:06 +0200

    2D portfolio

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I work mostly in JavaScript/ experiment with Flutter these days.
+      . I also like Golang and Python. Anyway regardless of the language, I just like programming.
+      Here is my <a href="https://github.com/Nusaibah1" target="_blank">Github</a>!`,
+    "cs-degree": `CS degree incoming...I'm hoping to expand my theoretical knowledge of computer science.`,
+    "sofa-table": `That's my sofa. I like to relax here and read comics. My current reading list includes Eleceed, Maru is a puppy and Special Civil Servant.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+     <a href="https://www.youtube.com/@nashallery" target="_blank">Nashallery</a>, <a href="https://www.youtube.com/@freecodecamp">Free Code Camp</a>,
+    <a href="https://www.youtube.com/@thecodedose" target="_blank">The Code Dose</a> and <a href="https://www.youtube.com/@DibyoshreeSarkar" target="_blank">DibyoshreeSarkar</a>!`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1EgogesgKb9u4tkjReXC5GMOomkZJiOZZ/view?usp=sharing" target="_blank">Check it out?</a>
+    Contact me at charifnusaibah01.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+    exit: `If you want to exit Nusaibah's portfolio, just close the tab.`,
+  };
